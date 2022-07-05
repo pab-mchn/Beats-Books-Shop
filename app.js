5fbc7383264a5c50a5990667b42d69e4380e10e8
pab-mich <pab.michelini@gmail.com>
2022-07-05 15:29:11 +0200

    added dinamic alert and stock products

--- app.js
+++ app.js
@@ -3,61 +3,9 @@ finishBuying = document.getElementById("finishBuying");
 carritoContent = document.getElementById("carrito-content");
 totalCarrito = document.getElementById("totalCarrito");
 seeCarrito = document.getElementById("seeCarrito");
+showAlert = document.getElementById("showAlert");
 
-const productos = [
-  {
-    id: 1,
-    name: "On The Road",
-    author: "Jack Kerouac",
-    price: 300,
-    img:
-      "https://imgs.search.brave.com/PG6VRGVRdd1tat50p4zbkf4eZejQVmLSXnyikv6sZfQ/rs:fit:346:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5z/eGVESlRoNnNFWDZk/VXhhcjJnTkNnSGFL/SiZwaWQ9QXBp",
-  },
-  {
-    id: 2,
-    name: "The Subterraneans",
-    author: "Jack Kerouac",
-    price: 300,
-    img:
-      "https://imgs.search.brave.com/xjuT1rKjFLtTvqpYLzm9WnQuJhjNsEYA_BjY0dc6DzY/rs:fit:169:225:1/g:ce/aHR0cHM6Ly90c2Uy/LmV4cGxpY2l0LmJp/bmcubmV0L3RoP2lk/PU9JUC56N0pWWExu/MlNRS2pnMFZJNU50/S1ZnQUFBQSZwaWQ9/QXBp",
-  },
-  {
-    id: 3,
-    name: "The Dharma Bums",
-    author: "Jack Kerouac",
-    price: 300,
-    img:
-      "https://imgs.search.brave.com/z18VKGHuzGbR8NKqeHitdlz1jLdJkAIJdFFNpiq3W-w/rs:fit:174:266:1/g:ce/aHR0cDovL2VjeC5p/bWFnZXMtYW1hem9u/LmNvbS9pbWFnZXMv/SS81MUJ4UmI4M2FI/TC5fU1kyNjRfQk8x/LDIwNCwyMDMsMjAw/X1FMNDBfLmpwZw",
-  },
-
-  {
-    id: 4,
-    name: "Junky",
-    author: "William Burroghs ",
-    price: 300,
-    img:
-      "https://imgs.search.brave.com/D2bD3wS_KSc2Gmt_vj7ptMfalnolARp_jqdJCdgpNAk/rs:fit:600:770:1/g:ce/aHR0cHM6Ly9pbWcu/ZGlzY29ncy5jb20v/NHlualBRbHd5R0Fr/Y3ZqRWpPVEFJZEs1/blgwPS9maXQtaW4v/NjAweDc3MC9maWx0/ZXJzOnN0cmlwX2lj/YygpOmZvcm1hdChq/cGVnKTptb2RlX3Jn/YigpOnF1YWxpdHko/OTApL2Rpc2NvZ3Mt/aW1hZ2VzL1ItMjYz/ODAzMS0xMjk0NDM2/NDcwLmpwZWcuanBn",
-  },
-
-  {
-    id: 5,
-    name: "Naked Lunch",
-    author: "William Burroghs ",
-    price: 300,
-    img:
-      "https://imgs.search.brave.com/53RKwp_6XkdBbQsMhwo5GmIx6afUjf2qIszchgbII-g/rs:fit:471:700:1/g:ce/aHR0cHM6Ly9yc3B1/bGwtc3VwZXJ2ZXJ0/Lm5ldGRuYS1zc2wu/Y29tL2ltYWdlcy9j/b3ZlcnMvbmFrZWRf/bHVuY2gvbmFrZWRf/bHVuY2gudWsuY2Fs/ZGVyLjE5NjQuanBn",
-  },
-
-  {
-    id: 6,
-    name: " Nova Express",
-    author: "William Burroghs ",
-    price: 300,
-    img:
-      "https://imgs.search.brave.com/C1aUiQuErxG0_mfx-JymphQ-1gvD3GZCzrpdQpClPyU/rs:fit:984:1200:1/g:ce/aHR0cHM6Ly8zLmJw/LmJsb2dzcG90LmNv/bS8tX3ZzMlg1eXJK/YlUvVXFEdXRQUGpk/NkkvQUFBQUFBQUFF/YWMvdnpKYWtQNVdj/XzAvczE2MDAvYnVy/cm91Z2hzLmpwZw",
-  },
-];
-
+//carrito start with the value of the localStorage content or an empty array
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 productos.forEach((product) => {
@@ -90,6 +38,7 @@ productos.forEach((product) => {
       price: product.price,
     });
 
+    createALalert();
     localStorage.setItem("carrito", JSON.stringify(carrito));
     console.log(carrito);
   });
@@ -97,37 +46,43 @@ productos.forEach((product) => {
 
 //function to paint carrito
 const paintCarritoElements = () => {
-  carritoContent.style.display = "block";
-  //clean and update carrito content
-  carritoContent.innerHTML = "";
+  if (carrito.length > 0) {
+    carritoContent.style.display = "block";
+    //clean and update carrito content
+    carritoContent.innerHTML = "";
 
-  carrito.forEach((product) => {
-    let contentCarrito = document.createElement("div");
+    carrito.forEach((product) => {
+      let contentCarrito = document.createElement("div");
 
-    contentCarrito.className = "modalContent";
+      contentCarrito.className = "modalContent";
 
-    contentCarrito.innerHTML = `
+      contentCarrito.innerHTML = `
             <img src="${product.img}"/>
             <h4>${product.author}</h4>
             <h4>${product.name}</h4>
             <h4>${product.price}$ </h4>
             `;
 
-    carritoContent.append(contentCarrito);
-    deleteProduct = document.createElement("i");
-    deleteProduct.className = "fa-solid fa-trash-can";
+      carritoContent.append(contentCarrito);
+      deleteProduct = document.createElement("i");
+      deleteProduct.className = "fa-solid fa-trash-can";
 
-    contentCarrito.append(deleteProduct);
+      contentCarrito.append(deleteProduct);
 
-    //call de function for delete item
-    deleteProduct.addEventListener("click", deleteProductItem);
-  });
-  //final price
-  const total = carrito.reduce((acc, el) => acc + el.price, 0);
-  const totalBuying = document.createElement("div");
-  totalBuying.className = "totalBuying";
-  totalBuying.innerHTML = `Total:  ${total} $`;
-  carritoContent.append(totalBuying);
+      //call de function for delete item
+      deleteProduct.addEventListener("click", deleteProductItem);
+    });
+
+    //final price calculation
+    const total = carrito.reduce((acc, el) => acc + el.price, 0);
+    const totalBuying = document.createElement("div");
+    totalBuying.className = "totalBuying";
+    totalBuying.innerHTML = `Total:  ${total} $`;
+    carritoContent.append(totalBuying);
+  } else {
+    //if carrito is empty, is not possible to see
+    carritoContent.style.display = "none";
+  }
 };
 
 // When the user clicks anywhere outside of the modal, close it
@@ -137,9 +92,30 @@ carritoContent.onclick = function (event) {
   }
 };
 
+//call the function to paint carrito
 seeCarrito.addEventListener("click", paintCarritoElements);
 
-// When the user clicks anywhere outside of the modal, close it;
+//create Alert Function after buy a book
+showAlert.style.display = "none";
+
+const createALalert = () => {
+  showAlert.innerHTML = "";
+  let contentAlert = document.createElement("span");
+
+  contentAlert.innerHTML = `
+  <span><strong>Yay!</strong> 🎉 your book has been added to your cart.</span>
+ 
+  `;
+  showAlert.append(contentAlert);
+
+  showAlert.style.display = "block";
+
+  function cleanAlert() {
+    showAlert.style.display = "none";
+  }
+
+  setTimeout(cleanAlert, 2000);
+};
 
 //function for delete product
 const deleteProductItem = () => {
@@ -156,6 +132,6 @@ const deleteProductItem = () => {
   //update LocalStorage
   localStorage.setItem("carrito", JSON.stringify(carrito));
 
-  //update carrito elemente after remove items
+  //update carrito elements after remove items
   paintCarritoElements();
 };
---
+++ data.js
@@ -0,0 +1,53 @@
+const productos = [
+  {
+    id: 1,
+    name: "On The Road",
+    author: "Jack Kerouac",
+    price: 300,
+    img:
+      "https://imgs.search.brave.com/PG6VRGVRdd1tat50p4zbkf4eZejQVmLSXnyikv6sZfQ/rs:fit:346:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5z/eGVESlRoNnNFWDZk/VXhhcjJnTkNnSGFL/SiZwaWQ9QXBp",
+  },
+  {
+    id: 2,
+    name: "The Subterraneans",
+    author: "Jack Kerouac",
+    price: 300,
+    img:
+      "https://imgs.search.brave.com/xjuT1rKjFLtTvqpYLzm9WnQuJhjNsEYA_BjY0dc6DzY/rs:fit:169:225:1/g:ce/aHR0cHM6Ly90c2Uy/LmV4cGxpY2l0LmJp/bmcubmV0L3RoP2lk/PU9JUC56N0pWWExu/MlNRS2pnMFZJNU50/S1ZnQUFBQSZwaWQ9/QXBp",
+  },
+  {
+    id: 3,
+    name: "The Dharma Bums",
+    author: "Jack Kerouac",
+    price: 300,
+    img:
+      "https://imgs.search.brave.com/z18VKGHuzGbR8NKqeHitdlz1jLdJkAIJdFFNpiq3W-w/rs:fit:174:266:1/g:ce/aHR0cDovL2VjeC5p/bWFnZXMtYW1hem9u/LmNvbS9pbWFnZXMv/SS81MUJ4UmI4M2FI/TC5fU1kyNjRfQk8x/LDIwNCwyMDMsMjAw/X1FMNDBfLmpwZw",
+  },
+
+  {
+    id: 4,
+    name: "Junky",
+    author: "William Burroghs ",
+    price: 300,
+    img:
+      "https://imgs.search.brave.com/D2bD3wS_KSc2Gmt_vj7ptMfalnolARp_jqdJCdgpNAk/rs:fit:600:770:1/g:ce/aHR0cHM6Ly9pbWcu/ZGlzY29ncy5jb20v/NHlualBRbHd5R0Fr/Y3ZqRWpPVEFJZEs1/blgwPS9maXQtaW4v/NjAweDc3MC9maWx0/ZXJzOnN0cmlwX2lj/YygpOmZvcm1hdChq/cGVnKTptb2RlX3Jn/YigpOnF1YWxpdHko/OTApL2Rpc2NvZ3Mt/aW1hZ2VzL1ItMjYz/ODAzMS0xMjk0NDM2/NDcwLmpwZWcuanBn",
+  },
+
+  {
+    id: 5,
+    name: "Naked Lunch",
+    author: "William Burroghs ",
+    price: 300,
+    img:
+      "https://imgs.search.brave.com/53RKwp_6XkdBbQsMhwo5GmIx6afUjf2qIszchgbII-g/rs:fit:471:700:1/g:ce/aHR0cHM6Ly9yc3B1/bGwtc3VwZXJ2ZXJ0/Lm5ldGRuYS1zc2wu/Y29tL2ltYWdlcy9j/b3ZlcnMvbmFrZWRf/bHVuY2gvbmFrZWRf/bHVuY2gudWsuY2Fs/ZGVyLjE5NjQuanBn",
+  },
+
+  {
+    id: 6,
+    name: " Nova Express",
+    author: "William Burroghs ",
+    price: 300,
+    img:
+      "https://imgs.search.brave.com/C1aUiQuErxG0_mfx-JymphQ-1gvD3GZCzrpdQpClPyU/rs:fit:984:1200:1/g:ce/aHR0cHM6Ly8zLmJw/LmJsb2dzcG90LmNv/bS8tX3ZzMlg1eXJK/YlUvVXFEdXRQUGpk/NkkvQUFBQUFBQUFF/YWMvdnpKYWtQNVdj/XzAvczE2MDAvYnVy/cm91Z2hzLmpwZw",
+  },
+];
